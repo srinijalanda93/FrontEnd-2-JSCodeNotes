26b289421e9515753f6e1769ec6728fd463cc153
srinija landa <srinijalanda@gmail.com>
2023-11-24 22:40:59 +0530

    added

---
+++ Assignment/javas.js
@@ -0,0 +1,42 @@
+// console.log("Hello world!");
+// console.log("srinija's frist assignment fighting!!")
+// var utcDate = new Date(Date.UTC(2023, 0, 17));
+// console.log(utcDate);
+/* PROGRAM -1
+Write a program that prints the numbers from 1 to 20 using alert.
+-For multiples of three print "Fizz" instead of the number.
+-For multiples of five print "Buzz" instead of the number.
+-For multiples of both three and five print "FizzBuzz".
+*/
+// let i;
+// for(i=0;i<=20;i++){
+//     if(i%3==0){
+//         console.log("Fizz");
+//     }else if(i%5==0){
+//         console.log("Buzz");
+//     }else if(i%3==0 && i%5==0){
+//         console.log("FizzBuzz");
+//     }
+// }
+
+/*DOM CURD OPERATION INSERT,UPDATE
+function changes(){
+	//before create ,append need to remove the p tag
+	const deleElePTag=document.querySelector("#status");
+	deleElePTag.remove();
+	//step 1 create the tagElement
+let headingTag=document.createElement("h1");
+//step -2 set the properties
+headingTag.innerText="Enter Metaverse";
+// step -3 create the parentnode obj, attach to the body(parent node)
+let parentElement=document.querySelector("body");
+//append the childelement to the parent
+//parentElement.appendChild(headingTag);
+// Append the child element to the parent
+parentElement.insertBefore(headingTag, document.getElementById("enterBtn"));
+	return parentElement;
+}
+**/
+
+// Capitalise Name
+
